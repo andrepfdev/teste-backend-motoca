05227e0af333ff83818f7e06c022f6ff74bb575e
andrepfdev <andrepfdev@gmail.com>
2026-04-21 01:51:56 -0300

    feat: reorganizar rotas de autenticação e proteger rotas de leads e veículos

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LeadController;
 use App\Http\Controllers\VehicleController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('leads', LeadController::class);
-Route::apiResource('vehicles', VehicleController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/leads', [LeadController::class, 'index']);
+    Route::apiResource('vehicles', VehicleController::class);
+});
