a109055f5df43616590297bcdabd630344109b07
andrepfdev <andrepfdev@gmail.com>
2026-04-21 11:52:27 -0300

    feat: aprimorar VehicleFactory para gerar modelos de veículos com base no tipo

--- database/factories/VehicleFactory.php
+++ database/factories/VehicleFactory.php
@@ -16,10 +16,16 @@ class VehicleFactory extends Factory
      */
     public function definition(): array
     {
+        $type = $this->faker->randomElement(VehicleType::cases())->value;
+
+        $models = $type === VehicleType::Motorcycle->value
+            ? ['CG 160 Titan', 'CB 300F Twister', 'CB 500 Hornet', 'CBR 650R', 'PCX 160', 'XRE 300 Sahara', 'Gold Wing']
+            : ['City Hatchback', 'Fit', 'Civic', 'Accord', 'HR-V', 'ZR-V', 'CR-V', 'WR-V', 'Pilot', 'Ridgeline', 'NSX', 'S2000', 'Integra Type R'];
+
         return [
-            'type' => $this->faker->randomElement(VehicleType::cases())->value,
-            'brand' => $this->faker->randomElement(['Honda', 'Toyota', 'Ford', 'Chevrolet', 'Volkswagen', 'Yamaha']),
-            'model' => $this->faker->word(),
+            'type' => $type,
+            'brand' => 'Honda',
+            'model' => $this->faker->randomElement($models),
             'year' => $this->faker->numberBetween(2000, (int) date('Y')),
             'price' => $this->faker->randomFloat(2, 5000, 150000),
             'color' => $this->faker->safeColorName(),
