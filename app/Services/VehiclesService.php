ea8762a31a149ecee4fd9035a4c546c646c03a21
andrepfdev <andrepfdev@gmail.com>
2026-04-21 00:27:45 -0300

    feat: implementar serviço VehiclesService para gerenciamento de veículos

---
+++ app/Services/VehiclesService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Vehicle;
+use App\Http\Resources\VehicleResource;
+
+class VehiclesService
+{
+    public function getAllVehicles()
+    {
+        return VehicleResource::collection(Vehicle::query()->simplePaginate(10));
+    }
+
+    public function getVehicleById($id)
+    {
+        $vehicle = Vehicle::findOrFail($id);
+        return new VehicleResource($vehicle);
+    }
+
+    public function createVehicle($data)
+    {
+        $vehicle = Vehicle::create($data);
+        return new VehicleResource($vehicle);
+    }
+
+    public function updateVehicle($id, $data)
+    {
+        $vehicle = Vehicle::findOrFail($id);
+        $vehicle->update($data);
+        return new VehicleResource($vehicle);
+    }
+
+    public function deleteVehicle($id)
+    {
+        $vehicle = Vehicle::findOrFail($id);
+        $vehicle->delete();
+        return response()->json(['message' => 'Vehicle deleted successfully']);
+    }
+
+}
