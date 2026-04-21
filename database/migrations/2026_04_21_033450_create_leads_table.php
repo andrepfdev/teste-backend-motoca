8600f4f4d08790387c5123df33fe142ae96bef1b
andrepfdev <andrepfdev@gmail.com>
2026-04-21 00:49:14 -0300

    feat: corrigir estrutura da migração para criação da tabela leads

--- database/migrations/2026_04_21_033450_create_leads_table.php
+++ database/migrations/2026_04_21_033450_create_leads_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->unsignedBigInteger('vehicle_id');
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->onDelete('cascade');
+            $table->text('message')->nullable();
             $table->timestamps();
         });
     }
